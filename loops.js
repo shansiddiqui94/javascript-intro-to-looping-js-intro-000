ded13d4cad4d8257f899c9290c132cc35e9739f6
Shan Siddiqui <shansiddiqui94@gmail.com>
2020-05-20 16:48:15 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -5,10 +5,9 @@ function forLoop(array){
 return [array, i]
 }
 
-function whileLoop(n){
-  var done = 0
-  while (n > done) {
-    n-=1
-    }
-  return n
+function whileLoop (number){
+  while (number>0){
+    console.log(--number)
+  }
+  return 'done'
 }
